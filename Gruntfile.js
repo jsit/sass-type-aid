8564263d41dea18f7027c649097d93ab38be29dc
Jay Sitter <jsitter@gmail.com>
2017-03-06 08:28:51 -0500

    Lots of changes

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		sassdoc: {
+			default: {
+				src: 'sass',
+				options: {
+					dest: 'docs/sass',
+					theme: 'flippant',
+				}
+			}
+		},
+		sass: {
+			expanded: {
+				options: {
+					style: 'expanded',
+					sourcemap: true
+				},
+				files: {
+					'example.css': 'example.scss'
+				}
+			},
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-sassdoc');
+	grunt.loadNpmTasks('grunt-sass');
+
+	// Default task.
+	grunt.registerTask('default', ['sassdoc', 'sass']);
+
+};
